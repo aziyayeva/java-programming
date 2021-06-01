afa15d6c9b9707e10cecc051dba601959ef819d7
Anna Zi <aziyayeva@gmail.com>
2021-06-01 15:34:06 -0400

    added Student class and Object for constructor

---
+++ src/day47_constructors/StudentObjects.java
@@ -0,0 +1,11 @@
+package day47_constructors;
+
+public class StudentObjects {
+    public static void main(String[] args) {
+        Student student1 = new Student(); // runs
+        Student student2 = new Student();
+        Student student3 = new Student("Anna");
+        Student student4 = new Student("Alihan");
+        Student student5 = new Student("Nasiba", 28);
+    }
+}
